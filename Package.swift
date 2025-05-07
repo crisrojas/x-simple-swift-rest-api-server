e48452132af240ab75c4c0c7dc0fbc18c7ce6be0
Cristian Rojas <cristian.rojas@live.fr>
2025-05-07 19:25:25 +0200

    Clean comments form package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,6 @@ import PackageDescription
 let package = Package(
     name: "x-simple-swift-rest-api-server",
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "x-simple-swift-rest-api-server"),
         .testTarget(
